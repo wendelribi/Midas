6a436f71819874ae993c75b2b1cb68ea94753621
ismael96 <140083162@aluno.unb.br>
2015-09-24 13:58:39 -0300

    Update and rename CadastrodeUsuario.java to Usuario.java
    Muda o nome do arquivo para facilitar a compilação.
    Acrescenta algumas bibliotecas necessárias.

--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.awt.*;
+
 import java.swing.*;
 
 // O codigo está bem seco, mas já podemos iniciar o modelo de como o usuario irá se cadastrar para o MIDAS //
