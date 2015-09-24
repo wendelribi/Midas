6da753bb09eb0d95b5e9e5b53726c03d6958d06e
ismael96 <140083162@aluno.unb.br>
2015-09-24 13:40:39 -0300

    Update and rename Cadastro de Usuario to CadastrodeUsuario.java
    Extensão .java.
    Importa a biblioteca swing. E retira import desnecessário

--- CadastrodeUsuario.java
+++ CadastrodeUsuario.java
@@ -1,7 +1,5 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.*;
+import java.swing.*;
 
 // O codigo está bem seco, mas já podemos iniciar o modelo de como o usuario irá se cadastrar para o MIDAS //
 
