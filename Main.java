37b2b8247eb97cb393361d26c2265ca27e025e63
iurebrandao <iurebrandao@gmail.com>
2015-09-29 19:51:50 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Botao_Usuario buttonFrame = new Botao_Usuario();
-		buttonFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		buttonFrame.setSize(275,110);
-		buttonFrame.setVisible(true);
-		
+		buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buttonFrame.setSize(500,110);
+		buttonFrame.setVisible(true);	
 	}
-
 }
