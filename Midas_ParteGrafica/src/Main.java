f7c582520e5f8704aa68c29b4464fc9d6679013a
iurebrandao <iurebrandao@aluno.unb.br>
2015-10-02 07:55:41 -0300

    Parte Grafica para rodar no Eclipse

---
+++ Midas_ParteGrafica/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Botao_Usuario buttonFrame = new Botao_Usuario();
+		buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buttonFrame.setSize(500,200);
+		buttonFrame.setVisible(true);	
+	}
+}
