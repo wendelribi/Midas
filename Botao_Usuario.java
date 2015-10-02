160f79273e7066c4111e4157a7c83af13c329a81
iurebrandao <iurebrandao@gmail.com>
2015-10-01 23:00:27 -0300

    Update Botao_Usuario.java

--- Botao_Usuario.java
+++ Botao_Usuario.java
@@ -7,21 +7,25 @@ import javax.swing.*;
 public class Botao_Usuario extends JFrame{
 	private JButton possui_cadastro; //botao que seleciona se a pessoa possui cadastro
 	private JButton nao_possui_cadastro;
-	private JTextField selecione;
+	private JLabel selecione;
+	private JOptionPane frame;
 	
 	public Botao_Usuario(){
 		super("Opções de usuario");
-		setLayout(new FlowLayout()); //configura o layout de frame
-		this.setLocation(400,300); //Posicionar o layout no lugar desejado da tela
+		this.setLayout(null);
+		this.setLocation(450,200); //Posicionar o layout no lugar desejado da tela
 		
-		selecione = new JTextField("\t    Selecione uma das opções:",30);
+		selecione = new JLabel("Selecione uma das opções:");
+		selecione.setBounds(170, 20, 300, 30);
 		add(selecione);
-		selecione.setEditable(false);
+		
 		
 		possui_cadastro = new JButton("Já Possuo cadastro");
+		possui_cadastro.setBounds(40, 70, 200, 70);
 		add(possui_cadastro);
 		
 		nao_possui_cadastro = new JButton("Requisitar cadastro");
+		nao_possui_cadastro.setBounds(250, 70, 200, 70);
 		add(nao_possui_cadastro);
 		
 		//	Manipulador de evento de botao
@@ -29,7 +33,11 @@ public class Botao_Usuario extends JFrame{
 		nao_possui_cadastro.addActionListener(handler);
 		possui_cadastro.addActionListener(handler);
 		
-		JOptionPane.showMessageDialog(null,"Bem-vindo ao MIDAS!");
+		JOptionPane telaInicial = new JOptionPane("          Bem-vindo ao MIDAS!");
+		JDialog dialogoTela = telaInicial.createDialog(null,"Tela Inicial");
+		dialogoTela.setLocation(600,200);
+		dialogoTela.setSize(200,200);
+		dialogoTela.setVisible(true);
 	}
 	
 //	Classe interna para tratamento de evento de botao
@@ -41,7 +49,7 @@ public class Botao_Usuario extends JFrame{
 				Botao_Usuario.this.dispose();				
 				Login_Usuario textFieldFrame = new Login_Usuario();
 				textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				textFieldFrame.setSize(350,100);
+				textFieldFrame.setSize(500,180);
 				textFieldFrame.setVisible(true);
 			}	
 			if(event.getSource() == nao_possui_cadastro ){
