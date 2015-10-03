196b9313fe502a764344e5caf4390f2af415e80d
iurebrandao <iurebrandao@gmail.com>
2015-10-03 12:47:15 -0300

    Update Confirmacao_Cadastro.java

--- Midas_ParteGrafica/src/Confirmacao_Cadastro.java
+++ Midas_ParteGrafica/src/Confirmacao_Cadastro.java
@@ -1,17 +1,20 @@
-
 public class Confirmacao_Cadastro {
 
+	// CLASSE QUE VERIFICA SE TODOS OS CAMPOS DOS CADASTROS ESTÃO VÁLIDAS
+
 	Confirmacao valida = new Confirmacao();
+	ValidacaoCpf cpfValido = new ValidacaoCpf();//CLASSE QUE VERIFICA O CPF
+	ValidacaoEmail emailValido = new ValidacaoEmail(); // CLASSE QUE VERIFICA EMAIL
+	
 
-	public boolean confirmacao(String senha, String senha2, String nome
-	, String sobrenome,String email,String cpf,boolean masc,boolean fem) {
+	public boolean confirmacao(String senha, String senha2, String nome, String sobrenome, String email, String cpf,
+			boolean masc, boolean fem) {
 
-		if (valida.confirmacaoSenha(senha, senha2) == true
-				& valida.camposValidos(nome, sobrenome, email, cpf) == true 
-				& valida.campoGenero(masc,fem)) {
+		if (valida.confirmacaoSenha(senha, senha2) == true & valida.camposValidos(nome, sobrenome, email, cpf) == true
+				& valida.campoGenero(masc, fem) & cpfValido.cpfValidacao(cpf) & emailValido.emailValidoo(email)) {
 			return true;
 		} else {
-			
+
 			return false;
 		}
 	}
