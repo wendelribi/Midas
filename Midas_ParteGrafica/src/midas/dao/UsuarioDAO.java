b7e9fbdcacde4ca27dfcb58d66df88d4aedd82df
ismael96 <140083162@aluno.unb.br>
2015-10-14 23:37:15 -0300

    Autorizar, Recuperar e IniciarOperações e finalizar

--- Midas_ParteGrafica/src/midas/dao/UsuarioDAO.java
+++ Midas_ParteGrafica/src/midas/dao/UsuarioDAO.java
@@ -2,9 +2,26 @@ package midas.dao;
 
 import java.awt.List;
 
+import javax.persistence.*;
+
 import midas.entidades.Usuario;
 
 public class UsuarioDAO {
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static EntityTransaction tx;
+	
+	public static void comecarOperacoes() {
+		emf = Persistence.createEntityManagerFactory("Midas");
+		em = emf.createEntityManager();
+		tx = em.getTransaction();
+		tx.begin();
+	}
+	public static void finalizarOperacoes(){
+		tx.commit();
+		em.close(); 
+		emf.close();
+	}
 	public boolean inserir(Usuario usuario) {
 		return false;
 	}
@@ -15,6 +32,19 @@ public class UsuarioDAO {
 		return new List();
 	}
 	public boolean autorizar(String cpf_usuario) {
-		return false;
+		Usuario usuario = recuperar(cpf_usuario);
+		if(usuario != null) {
+			usuario.setNivelDeAcesso(1);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public Usuario recuperar(String cpf_usuario){
+		try {
+			return em.find(Usuario.class, cpf_usuario);
+		} catch(IllegalArgumentException e) {
+			return null;
+		}
 	}
 }
