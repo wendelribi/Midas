c00680c0364fe366ac05e6c005b8e21036f0f305
LucasRez <macedolucas@hotmail.com>
2015-12-05 18:11:00 -0200

    Esqueleto do controller da imagem

--- src/main/java/midas/spring/mvc/controller/ImagemController.java
+++ src/main/java/midas/spring/mvc/controller/ImagemController.java
@@ -2,13 +2,18 @@ package midas.spring.mvc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-@RequestMapping("/")
+
+import midas.processamentoDeImagens.ProcessamentoDeImagens;
+import midas.processamentoDeImagens.ProcessamentodeImagensMarvin;
+@RequestMapping("/imagem")
 public class ImagemController {
 	
+	ProcessamentodeImagensMarvin processador;
 	
-	@RequestMapping ("/controller")
+	@RequestMapping ("/info")
 	public ModelAndView informacao()
 	{
+		//TODO Adicionar view da imagem com botao de fazer o gradiente
 		return new ModelAndView("/info/informacao");
 	}
 
